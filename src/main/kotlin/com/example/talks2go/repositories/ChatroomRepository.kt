83fa1b636164ce093a7a1dbe4a6121699bfda597
smgestupa <thereal.nwahs@gmail.com>
2023-07-04 13:36:58 +0800

    add function query to get specific chatroom ID between two students

--- src/main/kotlin/com/example/talks2go/repositories/ChatroomRepository.kt
+++ src/main/kotlin/com/example/talks2go/repositories/ChatroomRepository.kt
@@ -2,6 +2,17 @@ package com.example.talks2go.repositories
 
 import com.example.talks2go.models.Chatroom
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ChatroomRepository : JpaRepository<Chatroom, Int> {
+
+    @Query(
+        value = "SELECT room_id FROM chatroom WHERE first_student_email = :firstStudentEmail AND second_student_email = :secondStudentEmail",
+        nativeQuery = true
+    )
+    fun getChatroomID(
+        @Param("firstStudentEmail") firstStudentEmail: String,
+        @Param("secondStudentEmail") secondStudentEmail: String
+    ): Int
 }
