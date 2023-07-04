1f987fcb484cb6e4949b27270e3d6bfd95621e5f
smgestupa <thereal.nwahs@gmail.com>
2023-07-04 13:29:47 +0800

    implement saving messages to the database

--- src/main/kotlin/com/example/talks2go/controllers/ChatsController.kt
+++ src/main/kotlin/com/example/talks2go/controllers/ChatsController.kt
@@ -1,6 +1,13 @@
 package com.example.talks2go.controllers
 
+import com.example.talks2go.models.Message
+import com.example.talks2go.models.Student
+import com.example.talks2go.payloads.requests.LoginRequest
+import com.example.talks2go.payloads.requests.MessageRequest
+import com.example.talks2go.payloads.responses.DataResponse
 import com.example.talks2go.payloads.responses.MessageResponse
+import com.example.talks2go.repositories.ChatroomRepository
+import com.example.talks2go.repositories.MessageRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -8,21 +15,31 @@ import org.springframework.web.bind.annotation.*
 @CrossOrigin(origins=["*"], maxAge=3600)
 @RestController
 @RequestMapping("/api/v1/chats")
-class ChatsController {
-//
-//    @PostMapping(
-//            value = ["/get"],
-//            consumes = ["application/json"],
-//            produces = ["application/json"]
-//    )
-//    @ResponseBody
-//    @Throws(Exception::class)
-//    suspend fun retrieveBookMessages(@RequestBody ): ResponseEntity<Any> {
-//        var status: HttpStatus = HttpStatus.NOT_FOUND;
-//
-//        return ResponseEntity(
-//                MessageResponse("Something went wrong with retrieving chat messages", status.value()),
-//                status
-//        );
-//    }
+class ChatsController constructor(
+    val messageRepository: MessageRepository,
+    val chatroomRepository: ChatroomRepository
+) {
+    @PostMapping(
+        value = ["/message/send"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
+    )
+    @ResponseBody
+    @Throws(Exception::class)
+    fun sendMessageToChatroom(@RequestBody messageRequest: MessageRequest): ResponseEntity<Any> {
+        var status: HttpStatus = HttpStatus.OK;
+
+        val message: Message = Message(
+            roomID = messageRequest.roomID,
+            studentEmail = messageRequest.studentEmail,
+            content = messageRequest.content
+        );
+
+        messageRepository.save(message);
+
+        return ResponseEntity(
+            MessageResponse("Message has been saved to the database", status.value()),
+            status
+        );
+    }
 }
