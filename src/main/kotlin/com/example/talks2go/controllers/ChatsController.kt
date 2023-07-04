ed1e1f302444d056ea31d1e2d54f0daade4d917c
smgestupa <thereal.nwahs@gmail.com>
2023-07-04 13:55:16 +0800

    implement getting the chatroom ID and retrieving chatroom messages endpoints

--- src/main/kotlin/com/example/talks2go/controllers/ChatsController.kt
+++ src/main/kotlin/com/example/talks2go/controllers/ChatsController.kt
@@ -1,7 +1,9 @@
 package com.example.talks2go.controllers
 
+import com.example.talks2go.models.Chatroom
 import com.example.talks2go.models.Message
 import com.example.talks2go.models.Student
+import com.example.talks2go.payloads.requests.ChatroomRequest
 import com.example.talks2go.payloads.requests.LoginRequest
 import com.example.talks2go.payloads.requests.MessageRequest
 import com.example.talks2go.payloads.responses.DataResponse
@@ -19,6 +21,78 @@ class ChatsController constructor(
     val messageRepository: MessageRepository,
     val chatroomRepository: ChatroomRepository
 ) {
+    @PostMapping(
+        value = ["/chatroom"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
+    )
+    @ResponseBody
+    @Throws(Exception::class)
+    fun getChatroomID(@RequestBody chatroomRequest: ChatroomRequest): ResponseEntity<Any> {
+        var status: HttpStatus = HttpStatus.OK;
+
+        var chatroomID: Int? = null;
+        val chatroomIDThread = Thread {
+            chatroomID = chatroomRepository.getChatroomID(
+                chatroomRequest.firstStudentEmail,
+                chatroomRequest.secondStudentEmail
+            );
+        };
+
+        chatroomIDThread.start();
+        chatroomIDThread.join();
+
+        if (chatroomID == null) {
+            val chatroom: Chatroom = Chatroom(
+                firstStudentEmail = chatroomRequest.firstStudentEmail,
+                secondStudentEmail = chatroomRequest.secondStudentEmail
+            );
+
+            chatroomRepository.save(chatroom);
+
+            return ResponseEntity(
+                DataResponse(chatroom, status.value()),
+                status
+            );
+        }
+
+        return ResponseEntity(
+            DataResponse(chatroomRepository.findById(chatroomID!!).get(), status.value()),
+            status
+        );
+    }
+    @PostMapping(
+        value = ["/chatroom/list"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
+    )
+    @ResponseBody
+    @Throws(Exception::class)
+    fun listChatroomMessages(@RequestBody chatroomID: Int): ResponseEntity<Any> {
+        var status: HttpStatus = HttpStatus.OK;
+
+        var chatroomMessages: List<Message>? = null;
+        val chatroomMessagesThread = Thread {
+            chatroomMessages = messageRepository.getChatroomMessages(chatroomID);
+        };
+
+        chatroomMessagesThread.start();
+        chatroomMessagesThread.join();
+
+        if (chatroomMessages == null) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+            return ResponseEntity(
+                MessageResponse("Something went wrong with retrieving chat messages", status.value()),
+                status
+            );
+        }
+
+        return ResponseEntity(
+            DataResponse(chatroomMessages!!, status.value()),
+            status
+        );
+    }
     @PostMapping(
         value = ["/message/send"],
         consumes = ["application/json"],
--- src/main/kotlin/com/example/talks2go/repositories/MessageRepository.kt
+++ src/main/kotlin/com/example/talks2go/repositories/MessageRepository.kt
@@ -2,6 +2,14 @@ package com.example.talks2go.repositories
 
 import com.example.talks2go.models.Message
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface MessageRepository : JpaRepository<Message, Int> {
+
+    @Query(
+        value = "SELECT * FROM message WHERE room_id = :roomID",
+        nativeQuery = true
+    )
+    fun getChatroomMessages(@Param("roomID") roomID: Int): List<Message>;
 }
