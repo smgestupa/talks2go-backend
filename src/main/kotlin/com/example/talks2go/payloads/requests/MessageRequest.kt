172f9480864afa0cd44cadebaaa02aeefcb51c7f
smgestupa <thereal.nwahs@gmail.com>
2023-07-04 13:23:28 +0800

    position variables similar to the Message model

--- src/main/kotlin/com/example/talks2go/payloads/requests/MessageRequest.kt
+++ src/main/kotlin/com/example/talks2go/payloads/requests/MessageRequest.kt
@@ -1,7 +1,7 @@
 package com.example.talks2go.payloads.requests
 
 data class MessageRequest constructor(
-        val studentEmail: String,
         val roomID: Int,
+        val studentEmail: String,
         val content: String
 )
