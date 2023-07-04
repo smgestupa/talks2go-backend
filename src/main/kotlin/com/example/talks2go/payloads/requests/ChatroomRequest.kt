efddf2e86bd38e57e7c755eda32b542f606af9e3
smgestupa <thereal.nwahs@gmail.com>
2023-07-04 13:40:23 +0800

    remove the 'roomID' variable

--- src/main/kotlin/com/example/talks2go/payloads/requests/ChatroomRequest.kt
+++ src/main/kotlin/com/example/talks2go/payloads/requests/ChatroomRequest.kt
@@ -1,7 +1,6 @@
 package com.example.talks2go.payloads.requests
 
 data class ChatroomRequest constructor(
-    val roomID: Int,
     val firstStudentEmail: String,
     val secondStudentEmail: String
 )
