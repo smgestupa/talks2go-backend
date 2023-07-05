f8025853820c6affed0a6a88cf0366603aa45278
smgestupa <thereal.nwahs@gmail.com>
2023-07-05 21:12:05 +0800

    implement queries related to setting & removing students as logged in, selecting logged in students

--- src/main/kotlin/com/example/talks2go/repositories/StudentRepository.kt
+++ src/main/kotlin/com/example/talks2go/repositories/StudentRepository.kt
@@ -2,6 +2,34 @@ package com.example.talks2go.repositories
 
 import com.example.talks2go.models.Student
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
 
+@Repository
 interface StudentRepository : JpaRepository<Student, String> {
+
+    @Transactional
+    @Modifying
+    @Query(
+        value = "INSERT IGNORE logged_user VALUES (:studentEmail)",
+        nativeQuery = true
+    )
+    fun insertStudentAsLoggedIn(@Param("studentEmail") studentEmail: String);
+
+    @Transactional
+    @Modifying
+    @Query(
+        value = "DELETE FROM logged_user WHERE student_email = :studentEmail",
+        nativeQuery = true
+    )
+    fun deleteStudentAsLoggedIn(@Param("studentEmail") studentEmail: String);
+
+    @Query(
+        value = "SELECT student_email FROM logged_user",
+        nativeQuery = true
+    )
+    fun selectedLoggedInStudents(): List<String>;
 }
