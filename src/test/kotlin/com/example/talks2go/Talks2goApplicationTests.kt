2ae025b152890272f80dd9b3a43b61fd3b9e6658
smgestupa <thereal.nwahs@gmail.com>
2023-07-07 16:19:02 +0800

    comment out test

--- src/test/kotlin/com/example/talks2go/Talks2goApplicationTests.kt
+++ src/test/kotlin/com/example/talks2go/Talks2goApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class Talks2goApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 }
